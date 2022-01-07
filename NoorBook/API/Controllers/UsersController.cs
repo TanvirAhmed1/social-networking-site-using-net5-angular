bc45e9fff5c415cdf0e1d6f9f810e7b7cfcb5b01
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2022-01-08 01:28:28 +0600

    Removing ignored files

---
+++ NoorBook/API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> getUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<AppUser>> getUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
